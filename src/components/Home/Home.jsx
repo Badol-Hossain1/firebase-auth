1ca141cb1bde38cd8ccf8b0590c65fe413a77fd5
Badol-Hossain1 <badolhossain586.com>
2024-12-28 01:17:31 -0500

    firebase sdk added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <div>this is home</div>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from './components/Home/Home';
+import './index.css';
+import Main from './layout/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router= {router}/>
+  
   </StrictMode>,
 )
