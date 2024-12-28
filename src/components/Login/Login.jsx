17035d09f899837784f2d2a120a578326ee2b84e
Badol-Hossain1 <badolhossain586.com>
2024-12-28 05:37:25 -0500

    feat: added google login and logout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,49 @@
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import { getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
 import { GoogleAuthProvider } from 'firebase/auth';
+import { useState } from 'react';
 
 const Login = () => {
+  const [user, setUser] = useState(null);
+  console.log('🚀 ~ Login ~ user:', user);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
   const handleGoogleLogin = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user;
-        console.log('🚀 ~ handleGoogleLogin ~ user:', user);
+        const loginUser = result.user;
+        setUser(loginUser);
+        // console.log('🚀 ~ handleGoogleLogin ~ user:', loginUser);
       })
       .catch((error) => {
         console.log('🚀 ~ handleGoogleLogin ~ error:', error);
       });
   };
+
+  const hanleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log('🚀 ~ hanleSignOut ~ result:', result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log('🚀 ~ hanleSignOut ~ error:', error);
+      });
+  };
   return (
     <div>
-      <button onClick={handleGoogleLogin}>Google Login</button>
+      {user ? (
+        <button onClick={hanleSignOut}>SignOut</button>
+      ) : (
+        <button onClick={handleGoogleLogin}>Google Login</button>
+      )}
+      {user && (
+        <>
+          <p>user info</p>
+          {user?.email}
+          {user?.displayName}
+        </>
+      )}
     </div>
   );
 };
